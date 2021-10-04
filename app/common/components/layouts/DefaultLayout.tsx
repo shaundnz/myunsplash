cd3944e1706caa55fdf909f36d054f5095cf358e
Shaun Price <shaundnz@gmail.com>
2021-10-04 17:39:14 +1300

    Added page title and icon

--- app/common/components/layouts/DefaultLayout.tsx
+++ app/common/components/layouts/DefaultLayout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from "../modules/Header";
 import {Box} from "@chakra-ui/react";
 import SearchContextProvider from "../../context/SearchContext";
+import Head from "next/head";
 
 interface Props {
   children: React.ReactNode;
@@ -9,12 +10,19 @@ interface Props {
 
 const DefaultLayout: React.FC<Props> = ({children}) => {
   return (
+    <>
+    <Head>
+      <title>MyUnsplash</title>
+      <meta property="og:title" content="MyUnsplash" key="title" />
+      <link rel="icon" href="/unsplash-logo.png" />
+    </Head>
     <Box mt="4" mx={[0,0,8]}>
       <SearchContextProvider>
         <Header />
         {children}
       </SearchContextProvider>
     </Box>
+    </>
   );
 };
 
