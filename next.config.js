f4576dc15f011ec65766292322299923c184f0ae
Shaun Price <shaundnz@gmail.com>
2021-10-04 17:16:29 +1300

    Updated config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  target: "experimental-serverless-trace",
   webpack: (config, options) => {
     config.externals.push({
       "_http_common": "commonjs2 _http_common",
