89d0f55638f4472c1d2cba803e74fd05a35a8d6a
Shaun Price <shaundnz@gmail.com>
2021-10-04 14:59:49 +1300

    Updated webpack

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    config.externals.push({
+      "_http_common": "commonjs2 _http_common",
+    })
+    return config
+  }
 }
